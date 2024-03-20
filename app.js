8b86d1a0960e0b3e76a8f02c0cbc5e4cb529cfd6
Jayanth1312 <jayanthpaladugu3@gmail.com>
2024-03-20 14:18:31 +0530

    index.html

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'frontEnd')));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
